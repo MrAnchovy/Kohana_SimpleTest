bc40c35000853b69f968874bee0838a470d9f1aa
Mr Anchovy <mr.anchovy@mranchovy.com>
2011-08-02 00:25:36 +0100

    Remove @package from comment block in init.php to prevent claiming class Route!

--- init.php
+++ init.php
@@ -1,7 +1,5 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 /**
- * @package    Simpletest
- * @category   Kohana interface
  * @author     Mr Anchovy
  * @copyright  (c) 2011 Mr Anchovy
  * @license    http://opensource.org/licenses/ISC
